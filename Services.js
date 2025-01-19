cf1d985a51afe2160d3042847d0e0e9c7e33d954
primelifestyl3 <primelifestyl3@gmail.com>
2025-01-19 20:49:23 +0000

    Create Services.js

---
+++ Services.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Services.css';
+
+const Services = () => {
+  return (
+    <main>
+      <div className="services-container">
+        <div className="services-image">
+          <img src="/images/services.jpg" alt="Services" />
+        </div>
+        <div className="services-text">
+          <p>This is the text</p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Services;
