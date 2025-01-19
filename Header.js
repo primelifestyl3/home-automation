4554c4be5d9f931dabcdcb5bcb274f3ddf4971c1
primelifestyl3 <primelifestyl3@gmail.com>
2025-01-19 20:46:23 +0000

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <img src="/images/SmartQLiving Logo.png" alt="Logo" />
+      <h1>Smart IQ Living</h1>
+    </header>
+  );
+};
+
+export default Header;
