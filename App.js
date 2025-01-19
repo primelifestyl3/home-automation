7a8fcc7585ae8a95aad38c211df4a45df86de68d
primelifestyl3 <primelifestyl3@gmail.com>
2025-01-19 20:49:56 +0000

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Services from './pages/Services';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
