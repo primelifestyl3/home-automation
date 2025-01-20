a0348ec1f9c4225bff8db3d0705f43be2e71d533
primelifestyl3 <primelifestyl3@gmail.com>
2025-01-20 11:24:26 +0000

    Update Services.js

--- Services.js
+++ Services.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './Services.css';
+import 'Services.css';
 
 const Services = () => {
   return (
     <main>
       <div className="services-container">
         <div className="services-image">
-          <img src="/images/services.jpg" alt="Services" />
+          <img src="images/services.jpg" alt="Services" />
         </div>
         <div className="services-text">
           <p>This is the text</p>
