27bf3737922d82fd623ed27c1c2c46b6781c0038
primelifestyl3 <primelifestyl3@gmail.com>
2025-01-20 11:20:52 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Services from './pages/Services';
+import Header from 'components/Header';
+import Navigation from 'components/Navigation';
+import Footer from 'components/Footer';
+import Home from 'pages/Home';
+import Services from 'pages/Services';
 
-import './App.css';
+import 'App.css';
 
 const App = () => {
   return (
